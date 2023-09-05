49b8e350123f14898b7aa1a553d58c6aed4b9e2f
snturk <mcsnturk@gmail.com>
2023-09-05 20:57:53 +0300

    added username and password as env variable for default user initialization

--- internal/service/default_user_initializer.go
+++ internal/service/default_user_initializer.go
@@ -6,17 +6,18 @@ import (
 	"gin-jwt-boilerplate/internal/repository"
 	"gin-jwt-boilerplate/internal/utils"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"os"
 	"time"
 )
 
 // InitializeDefaultUser initializes the default user with default credentials
 func InitializeDefaultUser() error {
 	userCreateCmd := commands.CreateUserCommand{
-		Username: "lydia",
-		Password: "P@ssw0rd",
+		Username: os.Getenv("DEFAULT_USER_USERNAME"),
+		Password: os.Getenv("DEFAULT_USER_PASSWORD"),
 		PersonInfo: domain.PersonInfo{
-			FirstName: "Lydia",
-			LastName:  "Admin",
+			FirstName: "Default",
+			LastName:  "User",
 			BirthDate: primitive.
 				DateTime(time.Date(1990, 1, 1, 0, 0, 0, 0, time.UTC).UnixNano() / int64(time.Millisecond)),
 		},
